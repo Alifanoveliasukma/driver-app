e60a08af11fe4fcc1998bb71aef86e84f16516e2
Alifanoveliasukma <alifasukma11@gmail.com>
2025-08-07 10:12:36 +0700

    feat: create no order page

---
+++ resources/views/menu/no-order.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.template')
+
+@section('content')
+<div class="position-relative bg-purple text-white " style="height: 120px;">
+</div>
+
+<div class="d-flex flex-column justify-content-start align-items-center" style="height: calc(100vh - 60px); padding-top: 100px; padding-bottom: 20px;">
+
+  <img src="{{ asset('assets/icon/i-no-order.svg') }}" alt="truck" style="max-width: 250px;">
+
+  <h5 class="text-primary mt-3" style="font-weight: 600;">Tidak Ada Order!</h5>
+
+  <p class="text-muted text-center" style="font-size: 14px;">Harap tunggu penugasan<br>berikutnya dari kantor</p>
+</div>
+@endsection
