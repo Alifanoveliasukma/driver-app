4e852265fd0a151002e667676a9f0d95a1b7b351
Alifanoveliasukma <alifasukma11@gmail.com>
2025-08-18 11:40:12 +0700

    fix: fix route in web

--- routes/web.php
+++ routes/web.php
@@ -8,36 +8,17 @@ use App\Http\Controllers\Menu\HistoriController;
 use App\Http\Controllers\Menu\UjpController;
 use App\Http\Controllers\Menu\ProfilController;
 
-Route::get('/cek-ext-web', function () {
-    return [
-        'php_ini'   => php_ini_loaded_file(),
-        'pgsql'     => extension_loaded('pgsql'),
-        'pdo_pgsql' => extension_loaded('pdo_pgsql'),
-        'php'       => PHP_VERSION,
-    ];
-});
-
-Route::get('/diag-php', function () {
-    return [
-        'php_ini'   => php_ini_loaded_file(),
-        'scanned'   => php_ini_scanned_files(),         
-        'ext_dir'   => ini_get('extension_dir'),        
-        'pgsql'     => extension_loaded('pgsql'),
-        'pdo_pgsql' => extension_loaded('pdo_pgsql'),
-        'version'   => PHP_VERSION,
-    ];
-});
 
 Route::match(['get','post'], '/login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/konfirmasi-tiba-muat', function () {
-    return view('menu.utama.konfirmasi-tiba-muat');
-})->name('utama.konfirmasi-tiba-muat');
 
 Route::get('/no-order', function () {
     return view('menu.utama.no-order');
 });
+Route::get('/konfirmasi-tiba-muat', function () {
+    return view('menu.utama.konfirmasi-tiba-muat');
+})->name('utama.konfirmasi-tiba-muat');
 
 Route::get('/konfirmasi-selesai-muat', function () {
     return view('menu.utama.konfirmasi-selesai-muat');
