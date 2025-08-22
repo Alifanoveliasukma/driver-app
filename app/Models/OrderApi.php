1ba992fd1ef50105ddb20d7daf81f7c33fcf17a4
Alifanoveliasukma <alifasukma11@gmail.com>
2025-08-22 18:23:04 +0700

    refactor: move AuthApi and DriverApi to Services and add OrderService & OrderReadService

---
+++ app/Models/OrderApi.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderApi extends Model
+{
+    use HasFactory;
+}
