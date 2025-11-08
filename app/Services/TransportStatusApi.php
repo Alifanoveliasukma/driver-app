aa292ea2f19b0c4ea7c3f9691e9d9281552aa881
Alifanoveliasukma <alifasukma11@gmail.com>
2025-11-08 10:14:04 +0700

    feat: valid service transport status

--- app/Services/TransportStatusApi.php
+++ app/Services/TransportStatusApi.php
@@ -8,40 +8,49 @@ class TransportStatusApi extends BaseApi
      * Ambil semua data Transport Status (untuk role planner)
      */
     public function getAllTransportStatus()
-    {
-        $filter = "IsActive='Y'";
+{
+    $filter = "IsActive='Y' AND IsVoid='N'";
 
-        $request = '
+    $request = '
         <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:adin="http://3e.pl/ADInterface">
-           <soapenv:Header/>
-           <soapenv:Body>
-              <adin:queryData>
-                 <adin:ModelCRUDRequest>
-                    <adin:ModelCRUD>
-                       <adin:serviceType>API-TransportStatus</adin:serviceType>
-                       <adin:TableName>XX_TransportStatus</adin:TableName>
-                       <adin:Filter>' . $filter . '</adin:Filter>
-                       <adin:Action>Read</adin:Action>
-                       <adin:DataRow>
-                          <adin:field column="IsActive">
-                             <adin:val>Y</adin:val>
-                          </adin:field>
-                       </adin:DataRow>
-                    </adin:ModelCRUD>
-                    <adin:ADLoginRequest>
-                       <adin:user>' . env('ERP_USER') . '</adin:user>
-                       <adin:pass>' . env('ERP_PASS') . '</adin:pass>
-                       <adin:lang>192</adin:lang>
-                       <adin:ClientID>' . env('ERP_CLIENT') . '</adin:ClientID>
-                       <adin:RoleID>' . env('ERP_ROLE') . '</adin:RoleID>
-                       <adin:OrgID>' . env('ERP_ORG') . '</adin:OrgID>
-                       <adin:WarehouseID>' . env('ERP_WH') . '</adin:WarehouseID>
-                    </adin:ADLoginRequest>
-                 </adin:ModelCRUDRequest>
-              </adin:queryData>
-           </soapenv:Body>
+            <soapenv:Header/>
+            <soapenv:Body>
+                <adin:queryData>
+                    <adin:ModelCRUDRequest>
+                        <adin:ModelCRUD>
+                            <adin:serviceType>API-TransportStatus</adin:serviceType>
+                            <adin:TableName>XX_TransportStatus</adin:TableName>
+                            <adin:Filter>' . $filter . '</adin:Filter>
+                            <adin:Action>Read</adin:Action>
+                            <adin:DataRow>
+                                <adin:field column="IsActive">
+                                    <adin:val>Y</adin:val>
+                                </adin:field>
+                            </adin:DataRow>
+                        </adin:ModelCRUD>
+                        <adin:ADLoginRequest>
+                            <adin:user>' . env('ERP_USER') . '</adin:user>
+                            <adin:pass>' . env('ERP_PASS') . '</adin:pass>
+                            <adin:lang>192</adin:lang>
+                            <adin:ClientID>' . env('ERP_CLIENT') . '</adin:ClientID>
+                            <adin:RoleID>' . env('ERP_ROLE') . '</adin:RoleID>
+                            <adin:OrgID>' . env('ERP_ORG') . '</adin:OrgID>
+                            <adin:WarehouseID>' . env('ERP_WH') . '</adin:WarehouseID>
+                        </adin:ADLoginRequest>
+                    </adin:ModelCRUDRequest>
+                </adin:queryData>
+            </soapenv:Body>
         </soapenv:Envelope>';
 
-        return $this->sendRequest($request);
-    }
+    // Debug request
+    \Log::info('SOAP Request:', ['request' => $request]);
+    
+    $response = $this->sendRequest($request);
+    
+    // Debug response  
+    \Log::info('SOAP Response:', ['response' => $response]);
+    
+    return $response;
+}
+
 }
